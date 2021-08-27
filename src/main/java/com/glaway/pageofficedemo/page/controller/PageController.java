271932b905e07a207bea498f2753efff37b4c1ed
guiqingle520 <g1658411465@outlook.com>
2021-08-27 16:47:44 +0800

    1.测试1

--- src/main/java/com/glaway/pageofficedemo/page/controller/PageController.java
+++ src/main/java/com/glaway/pageofficedemo/page/controller/PageController.java
@@ -118,7 +118,7 @@ public class PageController {
      */
     @RequestMapping(value = "/save")
     public void saveFile(HttpServletRequest request, HttpServletResponse response){
-        System.out.println("233");
+        System.out.println("2334");
         FileSaver fs = new FileSaver(request, response);
         fs.saveToFile("d:\\" + fs.getFileName());
         fs.setCustomSaveResult("ok");//设置保存结果
